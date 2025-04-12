9ca2929f2145c797d7721b579c62b57583712914
Walther Oliveira <waltherpires@hotmail.com>
2025-04-11 21:42:57 -0300

    remove botao de voltar da tela de login

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -1,10 +1,17 @@
 // Dentro de LoginScreen.tsx
 import { View, Button, XStack, Image } from "tamagui";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useNavigationState } from "@react-navigation/native";
 import { ArrowLeft } from "lucide-react-native";
 
 export default function Header({ title }: { title?: string }) {
   const navigation = useNavigation();
+
+  const rotaAtual = useNavigationState((state) => {
+    const route = state.routes[state.index];
+    return route.name;
+  });
+
+  let onLoginScreen = rotaAtual == "Login";
 
   return (
     <XStack
@@ -13,13 +20,18 @@ export default function Header({ title }: { title?: string }) {
       bg="$blue10"
       alignItems="center"
       justifyContent="space-between"
+      zIndex={10}
     >
-      <Button
-        icon={ArrowLeft}
-        onPress={() => navigation.goBack()}
-        size="$3"
-        circular
-      />
+      {onLoginScreen ? (
+        <View width={40} />
+      ) : (
+        <Button
+          icon={ArrowLeft}
+          onPress={() => navigation.goBack()}
+          size="$3"
+          circular
+        />
+      )}
       <Image
         source={require("../assets/logo.png")}
         width={50}
