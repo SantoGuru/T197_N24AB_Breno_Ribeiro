33034a32ba11ec5e8ef5bdbd55a69a607a8eea62
Walther Oliveira <waltherpires@hotmail.com>
2025-04-15 02:36:11 -0300

    adiciona conexao supabase e modoficacoes logout e login

---
+++ my-app/src/lib/supabase.ts
@@ -0,0 +1,8 @@
+// lib/supabase.ts
+import { createClient } from '@supabase/supabase-js'
+import { SUPABASE_KEY, SUPABASE_URL } from 'app.config'
+
+const supabaseUrl = SUPABASE_URL
+const supabaseAnonKey = SUPABASE_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
--- my-app/src/navigation/Navigation.tsx
+++ my-app/src/navigation/Navigation.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import { useAuth } from "@/hooks/useAuth";
 import CreateAccount from "@/screens/CreateAccount";
 import LoginScreen from "@/screens/LoginScreen";
 import BottomTabs from "./BottomTabs";
 import { RootStackParamList } from "@/types/types";
+import Loading from "@/components/Loading";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function Navigation() {
+  const { user, loading } = useAuth();
+
+  if (loading) return <Loading>Aguarde...</Loading>;
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -18,17 +23,22 @@ export default function Navigation() {
           headerShown: false,
         }}
       >
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ title: "Entrar" }}
-        />
-        <Stack.Screen
-          name="CreateAccount"
-          component={CreateAccount}
-          options={{ title: "Criar Conta" }}
-        />
-        <Stack.Screen name="Tabs" component={BottomTabs} />
+        {user ? (
+          <Stack.Screen name="Tabs" component={BottomTabs} />
+        ) : (
+          <>
+            <Stack.Screen
+              name="Login"
+              component={LoginScreen}
+              options={{ title: "Entrar" }}
+            />
+            <Stack.Screen
+              name="CreateAccount"
+              component={CreateAccount}
+              options={{ title: "Criar Conta" }}
+            />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
